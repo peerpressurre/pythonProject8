7534b318e52f1263b04f9f2c2eae6963d9933101
katet0k <katakostucenko0@gmail.com>
2022-10-08 12:45:18 +0300

    hm3

--- main.py
+++
@@ -1,9 +0,0 @@
-try:
-    sign = input('Enter sigh->')
-    length = int(input('Enter length->'))
-
-    for x in range(0,length):
-         print(sign)
-
-except Exception as ex:
-    print(f'Error',{ex})
