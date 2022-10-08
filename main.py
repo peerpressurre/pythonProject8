b9500cef5624f0394673da3f9b844310477cffa2
katet0k <katakostucenko0@gmail.com>
2022-10-08 13:06:59 +0300

    hm3

---
+++ main.py
@@ -0,0 +1,14 @@
+try:
+    while True:
+        num = int(input('enter number->'))
+        if num != 7:
+            if num > 0:
+                print('Number is positive')
+            if num < 0:
+                print('Number is negative')
+            if num == 0:
+                print('Number is equal to zero')
+        else:
+            print('Good bye')
+except Exception as ex:
+    print(f'Error:', {ex})
